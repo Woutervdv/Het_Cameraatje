b1c6d155d4635891eb40fdf6834a7a641d4b2987
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-04-24 21:11:59 +0200

    Button renderer Added

---
+++ Het_Cameraatje/Het_Cameraatje.Android/CustomButtonRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Android.Content;
+using Het_Cameraatje.Controls;
+using Het_Cameraatje.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomButton), typeof(CustomButtonRenderer))]
+namespace Het_Cameraatje.Droid.Renderers
+{
+    class CustomButtonRenderer : ButtonRenderer
+    {
+        public CustomButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+
+        }
+    }
+}
---
+++ Het_Cameraatje/Het_Cameraatje/Controls/CustomButton.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace Het_Cameraatje.Controls
+{
+    public class CustomButton : Button
+    {
+    }
+}
