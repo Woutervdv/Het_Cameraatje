157b74a4d6a8bde06dea0020fa3c6b3b52c656de
Woutervdv <woutervandevorst@outlook.com>
2018-04-25 11:29:29 +0200

    work

--- Het_Cameraatje/Het_Cameraatje/ViewModels/HomePageViewModel.cs
+++ Het_Cameraatje/Het_Cameraatje/ViewModels/HomePageViewModel.cs
@@ -1,23 +1,41 @@
-﻿using Prism.Commands;
+﻿using Firebase.Auth;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
 
 namespace Het_Cameraatje.ViewModels
 {
+
 	public class HomePageViewModel : ViewModelBase
     {
+        FirebaseAuthLink auth;
         private string enviroment;
 
+        public ICommand LogOutCommand { get; private set; }
+        public ICommand AlbumCommand { get; private set; }
+        public ICommand CameraCommand { get; private set; }
 
         public HomePageViewModel(INavigationService navigationService)
             :base(navigationService)
         {
+            LogOutCommand = new DelegateCommand(() =>
+            {
+                NavigationService.NavigateAsync("StartPage");
+            });
+
 
         }
 
+
+
+
+       
+
+        
 
 
         public override void OnNavigatedTo(NavigationParameters parameters)
@@ -25,6 +43,10 @@ namespace Het_Cameraatje.ViewModels
             if (parameters.ContainsKey("Environment"))
             {
                 enviroment = (string)parameters["Environment"];
+            }
+            if (parameters.ContainsKey("Auth"))
+            {
+                auth = (FirebaseAuthLink)parameters["Auth"];
             }
         }
     }
