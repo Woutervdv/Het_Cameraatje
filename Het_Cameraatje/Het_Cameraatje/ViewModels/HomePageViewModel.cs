727cdfb3673a195b6456d2e019c061785507e338
Woutervdv <woutervandevorst@outlook.com>
2018-04-25 22:33:12 +0200

    commands homepage

--- Het_Cameraatje/Het_Cameraatje/ViewModels/HomePageViewModel.cs
+++ Het_Cameraatje/Het_Cameraatje/ViewModels/HomePageViewModel.cs
@@ -2,33 +2,60 @@
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Input;
+using Het_Cameraatje.Repositories;
+using Het_Cameraatje.Contracts;
 
+/*
+ * created by Wouter on 25/04/2018
+ */
 namespace Het_Cameraatje.ViewModels
 {
 
 	public class HomePageViewModel : ViewModelBase
     {
+        private IPageDialogService dialogService;
         FirebaseAuthLink auth;
         private string enviroment;
-
+        
+       
         public ICommand LogOutCommand { get; private set; }
         public ICommand AlbumCommand { get; private set; }
         public ICommand CameraCommand { get; private set; }
+        public ICommand ReturnCommand { get; private set; }
 
-        public HomePageViewModel(INavigationService navigationService)
-            :base(navigationService)
+        public HomePageViewModel(INavigationService navigationService, IPageDialogService dialogService)
+            : base(navigationService)
         {
             LogOutCommand = new DelegateCommand(() =>
             {
                 NavigationService.NavigateAsync("StartPage");
             });
 
+            AlbumCommand = new DelegateCommand(() =>
+            {
+                dialogService.DisplayAlertAsync("Album", "word getoond", "ok");
+            });
 
+            CameraCommand = new DelegateCommand(() =>
+            {
+                dialogService.DisplayAlertAsync("Camera", "word getoond", "ok");
+            });
+
+            ReturnCommand = new DelegateCommand(() =>
+            {
+                NavigationService.GoBackAsync();
+            });
+
+           
+           
         }
+
+       
 
 
 
@@ -47,6 +74,7 @@ namespace Het_Cameraatje.ViewModels
             if (parameters.ContainsKey("Auth"))
             {
                 auth = (FirebaseAuthLink)parameters["Auth"];
+              
             }
         }
     }
