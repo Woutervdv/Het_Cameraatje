613e05a4a552a56077574a0b5fd79173cb735668
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-05-16 08:50:44 +0200

    corner list

---
+++ Het_Cameraatje/Het_Cameraatje/Views/CornerListPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Het_Cameraatje.Views
+{
+    public partial class CornerListPage : ContentPage
+    {
+        public CornerListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
