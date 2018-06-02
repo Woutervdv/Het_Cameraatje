64bcfd03f89de97e8e236648982e2dbb09bd04ec
Woutervdv <woutervandevorst@outlook.com>
2018-06-02 15:40:18 +0200

    2 pages + viewmodels

---
+++ CleanProject/CleanProject/ViewModels/CornerListPageViewModel.cs
@@ -0,0 +1,76 @@
+﻿using CleanProject.Classes;
+using CleanProject.Contracts;
+using CleanProject.Models;
+using CleanProject.Repositories;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CleanProject.ViewModels
+{
+    public class CornerListPageViewModel : ViewModelBase
+    {
+        ICameraatjeDbContext dbContext;
+        private ICameraatjeRepository repo;
+
+        private IList<Location> locations;
+        public IList<Location> Locations
+        {
+            get { return locations; }
+            set { SetProperty(ref locations, value); }
+        }
+
+        private Location selectedLocation;
+        public Location SelectedLocation
+        {
+            get { return selectedLocation; }
+            set
+            {
+                if (SetProperty(ref selectedLocation, value) && selectedLocation != null)
+                {
+                    var p = new NavigationParameters
+                    {
+                        { "Location", selectedLocation },
+                        { "Environment", environment },
+                        { "User", user },
+                        { "PictureUrl", pictureUrl }
+                    };
+                    NavigationService.NavigateAsync("PartnerListPage", p);
+                    selectedLocation = null;
+                }
+            }
+        }
+
+        private string environment;
+        private User user;
+        private string pictureUrl;
+
+        public CornerListPageViewModel(INavigationService navigationService, ICameraatjeRepository repo, ICameraatjeDbContext dbContext) : base(navigationService)
+        {
+
+            this.repo = repo;
+            this.dbContext = dbContext;
+        }
+
+        public async override void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("Environment"))
+            {
+                environment = (string)parameters["Environment"];
+            }
+            if (parameters.ContainsKey("User"))
+            {
+                user = (User)parameters["User"];
+            }
+            if (parameters.ContainsKey("PictureUrl"))
+            {
+                pictureUrl = (string)parameters["PictureUrl"];
+            }
+            repo = new CameraatjeRepository(dbContext);
+            Locations = await repo.GetLocations();
+        }
+    }
+}
---
+++ CleanProject/CleanProject/ViewModels/KidListPageViewModel.cs
@@ -0,0 +1,77 @@
+﻿using CleanProject.Classes;
+using CleanProject.Contracts;
+using CleanProject.Models;
+using CleanProject.Repositories;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CleanProject.ViewModels
+{
+    public class KidListPageViewModel : ViewModelBase
+    {
+        private ICameraatjeDbContext dbContext;
+        private ICameraatjeRepository repo;
+
+        private IList<Kid> kids;
+        public IList<Kid> Kids
+        {
+            set { SetProperty(ref kids, value); }
+            get { return kids; }
+        }
+
+        private Kid selectedKid;
+        public Kid SelectedKid
+        {
+            get { return selectedKid; }
+            set
+            {
+                if (SetProperty(ref selectedKid, value) && selectedKid != null)
+                {
+                    var p = new NavigationParameters
+                    {
+                        { "Kid", selectedKid },
+                        { "Environment", environment },
+                        { "User", user },
+                        { "PictureUrl", pictureUrl }
+                    };
+                    NavigationService.NavigateAsync("CornerListPage", p);
+                    selectedKid = null;
+                }
+            }
+        }
+
+        private string environment;
+        private string pictureUrl;
+        public User user { get; set; }
+
+        public KidListPageViewModel(INavigationService navigationService, ICameraatjeRepository repo, ICameraatjeDbContext dbContext)
+          : base(navigationService)
+        {
+            this.dbContext = dbContext;
+            this.repo = repo;
+        }
+
+        public async override void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("Environment"))
+            {
+                environment = (string)parameters["Environment"];
+            }
+            if (parameters.ContainsKey("User"))
+            {
+                user = (User)parameters["User"];
+            }
+            if (parameters.ContainsKey("PictureUrl"))
+            {
+                pictureUrl = (string)parameters["PictureUrl"];
+            }
+
+            repo = new CameraatjeRepository(dbContext);
+            Kids = await repo.GetKids();
+        }
+    }
+}
